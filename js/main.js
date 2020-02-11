5e8efade27d9df139dbcfeb87780dc3bfba1cdb8
jakub_banasiak <jakub.banasiak@radpoint.pl>
2020-02-11 20:52:06 +0100

    total basic start

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ 
+const toggleButton = document.querySelector(".nav-toggle-button");
+
+toggleButton.addEventListener("click", function() {
+    document.querySelector(".page-header").classList.toggle("nav-opened");
+}, false)
