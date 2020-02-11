533401c62a0585faec2994e471fe2a9772ace34c
jakub_banasiak <jakub.banasiak@radpoint.pl>
2020-02-11 21:12:40 +0100

    add script to navigation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ const toggleButton = document.querySelector(".nav-toggle-button");
 toggleButton.addEventListener("click", function() {
     document.querySelector(".page-header").classList.toggle("nav-opened");
 }, false)
+
+const imagesButton = document.querySelector(".images-button");
+
+imagesButton.addEventListener("click", function() {
+    console.log('click')
+    document.querySelector(".images").classList.remove("hidden");
+    document.querySelector(".about-me").classList.add("hidden");
+}, false)
+
+
+const aboutMeButton = document.querySelector(".about-me-button");
+
+aboutMeButton.addEventListener("click", function() {
+    console.log('click')
+    document.querySelector(".images").classList.add("hidden");
+    document.querySelector(".about-me").classList.remove("hidden");
+}, false)
