74241e06735d138aad701dc109745cd8b346f1d2
jakub_banasiak <jakub.banasiak@radpoint.pl>
2020-03-04 20:01:15 +0100

    add galery caching

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
 self.addEventListener('install', function(event) {
     console.log('[SW], install event', event);
+    event.waitUntil(
+        caches.open('static')
+            .then(function(cache) {
+                console.log('[SW] Precaching app shell');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/js/main.js',
+                    '/css/main.css',
+                    '/css/icons.css',
+                    '/images/logo3.png',
+                    '/images/about-photo.jpg',
+                    '/images/runmagedon.jpg',
+                    // 'https://img.siepomaga.pl/uploads/fundraise/photo/6028/logo.jpg',
+                    'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap',
+                ])
+            })
+    )
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,8 +26,22 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[SW], fetch event', event);
-    event.respondWith(fetch(event.request))
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request)
+                        .then(function(res) {
+                            return caches.open('dynamic')
+                                .then(function(cache) {
+                                    return cache.put(event.request.url, res.clone())
+                                })
+                        })
+                }
+            })
+    )
 });
 
 let deferredPrompt;
